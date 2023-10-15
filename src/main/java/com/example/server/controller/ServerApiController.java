eb463ad3a97b31e15707ff578c58e4840c5d9cf5
pinggu <duddns6393@gmail.com>
2023-10-15 21:39:09 +0900

    restTemplte use get,post

---
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -0,0 +1,35 @@
+package com.example.server.controller;
+
+import com.example.server.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/server")
+public class ServerApiController {
+
+  @GetMapping("/hello")
+  public User hello(@RequestParam String name, @RequestParam int age) {
+    User user = new User();
+    user.setName(name);
+    user.setAge(age);
+    return user;
+  }
+
+  @PostMapping("/user/{userId}/name/{userName}")
+  public User post(@RequestBody User user, @PathVariable int userId, @PathVariable String userName) {
+    log.info("userId : {}, userName: {}", userId, userName);
+    log.info("client req : {}", user);
+
+    return user;
+
+  }
+
+}
